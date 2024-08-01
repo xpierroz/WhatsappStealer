e03e405525a485c1723f74353741adb69711dc04
D4RKN0$6 <109553205+DARKNOSY@users.noreply.github.com>
2024-08-01 23:44:00 +0200

    Update builder.cmd

--- builder.cmd
+++ builder.cmd
@@ -2,6 +2,20 @@
 title Whatsapp Stealer Builder
 color 6
 
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed. Installing now...
+    bitsadmin /transfer "DownloadPython" /priority high https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe %temp%\python-3.12.4-amd64.exe
+    %temp%\python-3.12.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+    del %temp%\python-3.12.4-amd64.exe
+    python --version >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Python installation failed. Exiting...
+        pause
+        exit /b 1
+    )
+)
+
 py -m pip install --upgrade requests pystyle colorama
 py -m main
 
